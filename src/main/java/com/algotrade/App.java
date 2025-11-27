fdc91e0caa5e4f8e46577dc49ef666cc7d2a4482
Joseph <josephhe255@gmail.com>
2025-11-27 15:38:32 +0100

    set up main simulation environment

--- src/main/java/com/algotrade/App.java
+++ src/main/java/com/algotrade/App.java
@@ -1,13 +1,31 @@
 package com.algotrade;
 
+import com.algotrade.exchange.Exchange;
+import com.algotrade.metrics.LatencyMetrics;
+import com.algotrade.metrics.TradeMetrics;
+import com.algotrade.model.MarketData;
+import com.algotrade.pipeline.ExchangeOrderExecutor;
+import com.algotrade.pipeline.ExecutionThrottler;
+import com.algotrade.pipeline.MarketDataStream;
+import com.algotrade.pipeline.TradingPipeline;
+import com.algotrade.risk.MaxPositionRiskManager;
+import com.algotrade.risk.PositionManager;
+import com.algotrade.simulator.MarketDataGenerator;
+import com.algotrade.strategy.MeanReversionStrategy;
+
+import java.util.concurrent.TimeUnit;
+
 /**
- * Hello world!
- *
+ * Main application to run the algorithmic trading engine simulation.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        // --- 1. Set up the main simulation environment ---
+        System.out.println("--- Initializing Simulation Environment ---");
+        Exchange exchange = new Exchange();
+        TradeMetrics tradeMetrics = new TradeMetrics();
+        LatencyMetrics latencyMetrics = new LatencyMetrics();
+
+        System.out.println("Simulation setup will be completed in subsequent steps.");
     }
 }
